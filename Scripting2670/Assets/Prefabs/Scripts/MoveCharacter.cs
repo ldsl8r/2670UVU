340a321ef281c512cf7760fbb6e82e759db448aa
Logan Slater <lslater742@gmail.com>
2017-08-28 14:22:20 -0600

    Auto stash before merge of "LearningAssets" and "UVUPipeline/LearningAssets"

---
+++ Scripting2670/Assets/Prefabs/Scripts/MoveCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveCharacter : MonoBehaviour {
+
+	CharacterController cc;
+
+	Vector3 tempMove;
+
+	public float speed = 5;
+
+	void Start () {
+		cc = GetComponent<CharacterController>();
+		MoveInput.KeyAction =Move;
+	}
+
+
+	// Update is called once per frame
+	void Move (float _movement) {
+		print(_movement);
+		tempMove.x = _movement*speed*Time.deltaTime;
+		cc.Move(tempMove);
+	}
+}
