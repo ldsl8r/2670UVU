bdbfc061528339ebe05e9e24950bc339601408cb
Logan Slater <lslater742@gmail.com>
2017-09-01 16:08:13 -0600

    Jump Imput Mod

--- Scripting2670/Assets/Scripts/MoveCharacter.cs
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -9,16 +9,26 @@ public class MoveCharacter : MonoBehaviour {
 	CharacterController cc;
 	Vector3 tempMove;
     public float speed = 5;
+    public float gravity = 1;
+    public float jumpHeight = 0.2f;
 
     void Start () {
 		PlayButton.Play += OnPlay;
 		cc = GetComponent<CharacterController>();
 	}
 	void OnPlay () {
+		MoveInput.JumpAction = Jump;
 		MoveInput.KeyAction += Move;
 		PlayButton.Play -= OnPlay;
 			}
+
+	void Jump () {
+		print("Jump");
+		tempMove.y += jumpHeight;
+	}
+
 	void Move (float _movement) {
+		tempMove.y -= gravity*Time.deltaTime;
 		tempMove.x = _movement*speed*Time.deltaTime;
 		print("move");
 		cc.Move(tempMove);
