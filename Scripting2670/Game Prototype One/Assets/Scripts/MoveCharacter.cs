54cbecbecda4248bcd2bd87073b9c762cc1c4737
Logan Slater <lslater742@gmail.com>
2017-09-05 21:32:45 -0600

    Merge branch 'master' into origin/master
    Level Design

---
+++ Scripting2670/Game Prototype One/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class MoveCharacter : MonoBehaviour {
+
+	CharacterController cc;
+	Vector3 tempMove;
+    public float speed = 5;
+    public float gravity = 1;
+    public float jumpHeight = 0.2f;
+	public float jumpCount = 2;
+
+    void Start () {
+		cc = GetComponent<CharacterController>();
+		PlayButton.Play += OnPlay;
+	}
+
+	void OnPlay () {
+		MoveInput.JumpAction = Jump;
+		MoveInput.KeyAction += Move;
+		PlayButton.Play -= OnPlay;
+	}
+
+	void Jump () {
+		tempMove.y = jumpHeight;
+
+		if(cc.isGrounded == true){
+			jumpCount = 2;
+		}
+		if(jumpCount != 0){
+			tempMove.y = jumpHeight;
+			jumpCount -= 1;
+		}
+	}
+
+	void Move (float _movement) {
+		tempMove.y -= gravity*Time.deltaTime;
+		tempMove.x = _movement*speed*Time.deltaTime;
+		cc.Move(tempMove);
+	}
+}
