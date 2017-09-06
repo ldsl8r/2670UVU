359d597d26f6a5fb27f050b1fc2fc948a85b5a32
Logan Slater <lslater742@gmail.com>
2017-09-06 15:53:33 -0600

    Added Respawn Scripts

---
+++ Scripting2670/Game Prototype One/Assets/Scripts/EndGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class EndGame : MonoBehaviour {
+
+	public static Action End;
+
+	void OnTrigerEnter()
+	{
+		End();
+	}
+}
---
+++ Scripting2670/Game Prototype One/Assets/Scripts/Reset.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Reset : MonoBehaviour {
+
+public Transform startPoint;
+
+public GameObject art;
+
+
+
+void Start (){
+	EndGame.End += ResetThis;
+}
+
+
+	void ResetThis (){
+		art.SetActive(false);
+		transform.position = startPoint.position;
+		Invoke("Restart", 3);
+}
+
+	void Restart(){
+		art.SetActive(true);
+		transform.position = startPoint.position;
+	}
+
+}
