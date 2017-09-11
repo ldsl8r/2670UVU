ce4c573ad251019822bf6aad578bd94db4ca93e2
Logan Slater <lslater742@gmail.com>
2017-09-11 14:11:53 -0600

    Balloon AutoMove

---
+++ Scripting2670/Assets/AutoMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMove : MonoBehaviour {
+
+	public Vector3 moveIt;
+		
+	void Start()
+	{
+		StartCoroutine(BalloonFloat());
+	}
+	
+	// Update is called once per frame
+	IEnumerator BalloonFloat () {
+		while (moveIt.x < 0.3f) {
+			print("balloon has moved");
+			moveIt.x += 0.1f*Time.deltaTime;
+			transform.Translate(moveIt);
+			yield return new WaitForSeconds(0.01f);
+		}
+	}
+}
