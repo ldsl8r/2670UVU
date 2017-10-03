7c6cf18bdc0de65b056b230e387de8e38c80c188
Logan Slater <lslater742@gmail.com>
2017-10-02 21:33:33 -0600

    Building Temple second floor with trap pit

--- Scripting2670/Game Prototype One/Assets/Scripts/RespawnField.cs
+++ Scripting2670/Game Prototype One/Assets/Scripts/RespawnField.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RespawnField : MonoBehaviour {
+	Vector3 originalPos;
+	public float startDelay;
 
-public Transform respawnPoint;
-
-	void OnTriggerEnter(Collider other)
+void Start()
 	{
-		if(other.tag == "Player")
+	 originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+	}
+
+	void OnTriggerEnter()
+	{
+		StopAllCoroutines();
+		StartCoroutine(ResetOBJPosition());
+			
+	}
+
+	IEnumerator ResetOBJPosition()
+	{
+		yeild return new WaitForSeconds(startDelay);
+		while(transform.position !=originalPos)
 		{
-			other.transform.position = respawnPoint.position;
-		}	
+			gameObject.transform.position = originalPos;
+			yield return null;
+		}
 	}
 }
