f62751e69bf9ceaf4d00f6ab613182b8d665bccd
Logan Slater <lslater742@gmail.com>
2017-10-06 16:00:26 -0600

    Got HealthBar to work kind of

--- Scripting2670/Game Prototype One/Assets/Scripts/FallingPlatform.cs
+++ Scripting2670/Game Prototype One/Assets/Scripts/FallingPlatform.cs
@@ -22,6 +22,6 @@ public class FallingPlatform : MonoBehaviour {
 			transform.position.z);
 			yield return null;
 		}
-		gameObject.SetActive(false);
+		// gameObject.SetActive(false);
 	}
 }
---
+++ Scripting2670/Game Prototype One/Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour {
+
+	Vector3 originalPos;
+	public float startDelay;
+	void Start () {
+		originalPos = transform.position;
+	}
+		
+		void OnTriggerEnter()
+		{
+			StopAllCoroutines();
+			StartCoroutine(ResetOBJPosition());
+		}
+
+		IEnumerator ResetOBJPosition()
+		{
+			yield return new WaitForSeconds(startDelay);
+			while(transform.position != originalPos)
+			{
+				transform.position = originalPos;
+				yield return null;
+			}
+		}
+}
