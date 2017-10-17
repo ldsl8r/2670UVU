441ad023536e6d217cd0c7d8ea747fe624572628
Logan Slater <lslater742@gmail.com>
2017-10-17 12:44:27 -0600

    Added NavMesh and Jaguar Enemy making some areas inaccesable via box col

---
+++ Scripting2670/Game Prototype One/Assets/Scripts/AINavMesh/EnemyMove.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMove : MonoBehaviour {
+
+	public NavMeshAgent agent;
+	private Transform player;
+	private Transform TowardsMe;
+	public Transform Spot1;
+	public Transform Spot2;
+	public int location;
+	
+	void Awake ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		SendToEnemy.SendTransform += SendTransformHandler;
+		TowardsMe = Spot1;
+		StartCoroutine(Follow());
+		SendToEnemy.ReturnPatrol = ReturnHandler;
+
+		
+	}
+
+    private void ReturnHandler()
+    {
+        TowardsMe = Spot1;
+    }
+
+    private void SendTransformHandler(Transform _transform)
+    {
+        TowardsMe = _transform;
+    }
+
+    IEnumerator Follow () 
+	{
+		while (true)
+		{
+			yield return new WaitForFixedUpdate();
+			agent.destination = TowardsMe.position;
+			if(agent.transform.position == Spot1.transform.position){
+				TowardsMe = Spot2;
+			}
+			if(agent.transform.position == Spot2.transform.position){
+				TowardsMe = Spot1;
+			}
+		}
+	}	
+
+	// OnTriggerEnter (){
+	// 	switch (location){
+	// 		case 1:
+	// 			TowardsMe = Spot1;
+	// 			location = 2;
+	// 		break;
+	// 		case 2:
+	// 			TowardsMe = Spot2;
+	// 			location = 1;
+	// 		break;
+	// 	}
+	
+	
+}
---
+++ Scripting2670/Game Prototype One/Assets/Scripts/AINavMesh/SendToEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SendToEnemy : MonoBehaviour {
+
+	public static Action<Transform> SendTransform;
+	public Transform Player;
+	public static Action ReturnPatrol;
+	void OnTriggerEnter()
+	{
+		SendTransform(Player);
+	}
+	void OnTriggerExit()
+	{
+		ReturnPatrol();
+	}
+	
+}
