a7e47afd85ec84700a4c01d1aa18f8e0d443058a
Logan Slater <lslater742@gmail.com>
2017-10-23 22:11:01 -0600

    Made SplashScreen Fade in and out, started on pause screen

---
+++ Scripting2670/Game Prototype One/Assets/Scripts/PauseMenu1.cs
@@ -0,0 +1,45 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu1 : MonoBehaviour {
+
+	public bool paused = false;
+
+	
+	void Update()
+	{
+		if(Input.GetKeyDown("p"))
+		{
+			gamePaused();
+		}
+	}
+
+	void OnGUI()
+	{
+		if(paused == true)
+		{
+			GUILayout.Label("Game id Paused");
+			if(GUILayout.Button ("Click to Resume"))
+			{
+				gamePaused();
+			}
+		}
+	}
+	
+	IEnumerator gamePaused ()
+	{
+		if (Time.timeScale == 1)
+		{
+			Time.timeScale = 0
+			paused = true;
+		}
+
+		else
+		{
+			Time.timeScale = 1;
+			paused = false;
+		}
+	}
+}
+*/
