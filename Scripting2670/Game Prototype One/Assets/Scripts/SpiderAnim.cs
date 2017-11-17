1ac7a2e31f0df4f2d39f53461e43e1e15aa9e289
Logan Slater <lslater742@gmail.com>
2017-11-16 21:44:00 -0700

    Imported Spider, Made Controller and Script

---
+++ Scripting2670/Game Prototype One/Assets/Scripts/SpiderAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderAnim : MonoBehaviour {
+
+	private Animator animator;
+	private NavMeshAgent agent;
+	private bool Walking = true;
+	private float Speed;
+
+	void Start () {
+		animator = GetComponentInChildren<Animator>();
+		agent = GetComponent<NavMeshAgent>();
+		StartCoroutine(WalkSpeed());
+	}
+	
+	IEnumerator WalkSpeed()
+	{
+		while(Walking)
+		{
+			Speed = agent.velocity.x;
+			Speed = Mathf.Abs(Speed);
+			animator.SetFloat("Velocity" , Speed/3);
+			yield return new WaitForSeconds(.1f);
+		}
+	}
+}
