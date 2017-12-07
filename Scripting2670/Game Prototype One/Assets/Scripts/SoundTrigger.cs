0a1af517a6e0da4b700b8c5228597a31a01fa7f8
Logan Slater <lslater742@gmail.com>
2017-12-07 11:32:02 -0700

    imported shaders

---
+++ Scripting2670/Game Prototype One/Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundTrigger : MonoBehaviour {
+
+	public AudioSource SoundSource;
+	public AudioClip SoundFX;
+
+	private bool hasPlayedAudio;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (hasPlayedAudio == false)
+		{
+			SoundSource.PlayOneShot(SoundFX);
+			hasPlayedAudio = false;
+			 
+		}
+	}
+
+
+
+}
