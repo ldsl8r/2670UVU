2ee8000196cf7b19576c11cc2aa0a5ec9e23ff10
Logan Slater <lslater742@gmail.com>
2017-12-12 11:09:45 -0700

    worked on sounds for zones and enemys, footsteps and worked on textures

---
+++ Scripting2670/Game Prototype One/Assets/Scripts/FootStepSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepSound : MonoBehaviour {
+
+	public AudioSource FootStepSource;
+
+	public void playSound()
+	{
+		FootStepSource.Play();
+	}
+}
---
+++ Scripting2670/Game Prototype One/Assets/Scripts/SoundTriggerDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundTriggerDeath : MonoBehaviour {
+
+		public AudioSource Source;
+	public AudioClip OwNoise;
+
+	private bool hasPlayedAudio;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (hasPlayedAudio == false)
+		{
+			Source.PlayOneShot(OwNoise);
+			hasPlayedAudio = false;
+			 
+		}
+	}
+}
