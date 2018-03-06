2cfb008530bb5b258bb1c80590b3a83b40ac9534
Logan Slater <lslater742@gmail.com>
2018-03-06 12:17:48 -0700

    Deleted UNITE2017 except the scripts i needed.

---
+++ Animation State Machine Scriptable Object/Assets/Scripts/AnimBehaves.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimBehaves : StateMachineBehaviour {
+
+    // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+       // Debug.Log("Start");
+
+    //}
+
+	
+}
